55fe28c8d0d62f3fea8ecf64b2d450dd064aa3e0
Bheemesh Gouda <bheemeshgouda8@gmail.com>
2026-04-10 21:51:10 +0530

    add test controller

--- portfolio-backend/src/main/java/com/portfolio/portfolio_backend/PortfolioBackendApplication.java
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/PortfolioBackendApplication.java
@@ -2,12 +2,18 @@ package com.portfolio.portfolio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {
+        DataSourceAutoConfiguration.class,
+        HibernateJpaAutoConfiguration.class
+    }
+)
 public class PortfolioBackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PortfolioBackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(PortfolioBackendApplication.class, args);
+    }
 }
---
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.portfolio.portfolio_backend.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Backend working 🚀";
+    }
+}
