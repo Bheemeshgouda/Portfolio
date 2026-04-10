255ed2b3de24acf346a28b02154963dca5ef18d7
Bheemesh Gouda <bheemeshgouda8@gmail.com>
2026-04-10 23:34:36 +0530

    final db fix

--- portfolio-backend/src/main/java/com/portfolio/portfolio_backend/PortfolioBackendApplication.java
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/PortfolioBackendApplication.java
@@ -2,15 +2,12 @@ package com.portfolio.portfolio_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(
-    exclude = {
-        DataSourceAutoConfiguration.class,
-        HibernateJpaAutoConfiguration.class
-    }
-)
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.portfolio.portfolio_backend.repository")
+@EntityScan(basePackages = "com.portfolio.portfolio_backend.model")
 public class PortfolioBackendApplication {
 
     public static void main(String[] args) {
--- portfolio-backend/src/main/java/com/portfolio/portfolio_backend/repository/AboutRepository.java
+++ portfolio-backend/src/main/java/com/portfolio/portfolio_backend/repository/AboutRepository.java
@@ -2,6 +2,8 @@ package com.portfolio.portfolio_backend.repository;
 
 import com.portfolio.portfolio_backend.model.About;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AboutRepository extends JpaRepository<About, Long> {
 }
